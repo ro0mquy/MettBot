a666371a8b7f72e85504fe5d224a749de1a97d7a
Dominik Paulus <dominik@d-paulus.de>
2011-12-24 01:25:26 +0100

    Another test.go

--- test.go
+++ test.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"bufio"
 	"net"
+	"os"
 )
 
 func main() {
-	var sock net.Conn
+	var sock *net.TCPConn
 	if s, ret := net.Dial("tcp", "localhost:6667"); ret != nil {
-		fmt.Println("Error")
+		fmt.Println("Error: " + ret.String())
 		return
 	} else {
-		sock = s
+		sock, _ = s.(*net.TCPConn)
 	}
+
 	io := bufio.NewReadWriter(bufio.NewReader(sock),
 		bufio.NewWriter(sock))
-	io.WriteString("Hallo, Welt")
+	for {
+		var err os.Error
+		var in string
+		if in, err = io.ReadString('\n'); err != nil {
+			fmt.Println("Error! " + err.String())
+			return
+		}
+		if _, err = io.WriteString(in); err != nil {
+			fmt.Println("Error! " + err.String())
+			return
+		}
+		io.Flush()
+		fmt.Print(in)
+	}
 }
