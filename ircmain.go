8dcf4b238c2aefce1569b61fb148c67f054cab30
Dominik Paulus <dominik@d-paulus.de>
2012-01-01 17:24:53 +0100

    First work on rudimentary configuration system

--- ircmain.go
+++ ircmain.go
@@ -3,15 +3,44 @@ package main
 import (
 	"ircclient"
 	"log"
-//	"time"
+	"github.com/kless/goconfig/config"
 )
 
 func main() {
-	//for _, line := range server_lines {
-	//	ircclient.ParseServerLine(line)
-	//}
-	s := ircclient.NewIRCClient("dpaulus.dyndns.org:6667", "testbot", "testbot", "ident", '.')
-	ok := s.Connect()
+	c, ok := config.ReadDefault("go-faui2k11.cfg")
+	if ok != nil {
+		c = config.NewDefault()
+		c.AddSection("Server")
+		c.AddOption("Server", "host", "dpaulus.dyndns.org:6667")
+		c.AddOption("Server", "nick", "testbot")
+		c.AddOption("Server", "ident", "ident")
+		c.AddOption("Server", "realname", "TestBot Client")
+		c.AddOption("Server", "trigger", ".")
+		c.WriteFile("go-faui2k11.cfg", 0644, "go-faui2k11 default config file")
+		log.Println("Note: A new default configuration file has been generated in go-faui2k11.cfg. Please edit it to suit your needs and restart go-faui2k11 then")
+		return
+	}
+	options := make(map[string]string)
+	for _, x := range []string{"host", "nick", "ident", "realname"} {
+		it, err := c.String("Server", x)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		options[x] = it
+	}
+	var trigger byte
+	strigger, err := c.String("Server", "trigger")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if len(strigger) > 1 {
+		log.Fatal("Trigger must be exactly one byte long")
+	}
+
+	s := ircclient.NewIRCClient(options["host"], options["nick"], options["realname"], options["ident"], trigger)
+	ok = s.Connect()
 	if ok != nil {
 		log.Fatal(ok.String())
 	}
