8d8a1980dd5498c241059f32a0a99dc821ae4a54
Johannes Schilling <dario@zerties.org>
2012-01-02 01:32:41 +0100

    plugins: added listplugins

---
+++ plugins/listplugins.go
@@ -0,0 +1,60 @@
+package plugins
+
+import (
+	"ircclient"
+	"strings"
+	"log"
+	"fmt"
+)
+
+type ListPlugins struct {
+	ic *ircclient.IRCClient
+}
+
+func (lp *ListPlugins) Register(ic *ircclient.IRCClient) {
+	lp.ic= ic
+}
+
+func (lp ListPlugins) String() string {
+	return "listplugins"
+}
+
+func (lp ListPlugins) Info() string {
+	return "Lists all currently registered Plugins"
+}
+
+
+func (lp ListPlugins) ProcessLine(msg *ircclient.IRCMessage) {
+	return
+}
+
+/**
+ * the array-foo makes it easy to leave out the last ", "
+ * because strings.Join() does that for us
+ **/
+func (lp ListPlugins) ProcessCommand(cmd *ircclient.IRCCommand) {
+	if cmd.Command != "listplugins" {
+		return
+	}
+
+	a := make([]string, 0)
+	for name, _ := range lp.ic.GetPlugins() {
+		a= append(a, name)
+	}
+
+	// channel vs. query
+	var ret_targ string
+	if cmd.Target[0] == '#' {
+		ret_targ = cmd.Target
+	} else {
+		ret_targ = cmd.Source
+	}
+	ret := fmt.Sprintf("PRIVMSG %s :%s", ret_targ, strings.Join(a, ", "))
+	log.Println("listplugins output: " + ret)
+	lp.ic.SendLine(ret)
+}
+
+
+func (lp ListPlugins) Unregister() {
+	// nothing to do here
+}
