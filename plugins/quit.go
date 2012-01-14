ad14bae372e6cb5f18808d0a37f51f122143ecf8
Dominik Paulus <dominik@d-paulus.de>
2012-01-14 20:27:19 +0100

    Update quit.go to use new API

--- plugins/quit.go
+++ plugins/quit.go
@@ -1,16 +1,13 @@
-// quit command is not that much config-related,
-// and i wanted access control for it anyway, so
-// while i'm at it, i decided to put it into an extra file
-
 package plugins
 
 import (
 	"ircclient"
-	//"log"
+	"log"
+	"fmt"
 )
 
 const (
-	quit_min_auth_level = "300"
+	quit_min_auth_level = 300
 	default_quit_msg    = "Bye."
 )
 
@@ -20,6 +17,19 @@ type QuitHandler struct {
 
 func (q *QuitHandler) Register(ic *ircclient.IRCClient) {
 	q.ic = ic
+
+	if q.ic.GetStringOption("Quit", "quit_minlevel") == "" {
+		q.ic.SetIntOption("Quit", "quit_minlevel", quit_min_auth_level)
+		log.Println("added default quit_minlevel value of \"" + fmt.Sprintf("%d", quit_min_auth_level) + "\" to config file")
+		// no return either, sorry ;)
+	}
+
+	if q.ic.GetStringOption("Quit", "quitmsg") == "" {
+		log.Println("added default quitmsg value of \"" + default_quit_msg + "\" to config file")
+		q.ic.SetStringOption("Quit", "quitmsg", default_quit_msg)
+	}
+
+	q.ic.RegisterCommandHandler("quit", 0, quit_min_auth_level, q)
 }
 
 func (q *QuitHandler) String() string {
@@ -27,7 +37,7 @@ func (q *QuitHandler) String() string {
 }
 
 func (q *QuitHandler) Info() string {
-	return "handles the quit command with authentication"
+	return "handles the quit command"
 }
 
 func (q *QuitHandler) ProcessLine(msg *ircclient.IRCMessage) {
@@ -35,53 +45,7 @@ func (q *QuitHandler) ProcessLine(msg *ircclient.IRCMessage) {
 }
 
 func (q *QuitHandler) ProcessCommand(cmd *ircclient.IRCCommand) {
-	if cmd.Command != "quit" {
-		return
-	}
-	lvl := q.ic.GetAccessLevel(cmd.Source)
-
-	_ = lvl
-	// TODO!
-	/*
-		if ! q.config.Conf.HasSection("Quit") {
-			log.Println("no \"Quit\" section.. adding one for your convenience")
-			q.config.Conf.AddSection("Quit")
-			// no return here so the next if does its job as well and we have a
-			// working default config after just one failed attempt
-		}
-		if ! q.config.Conf.HasOption("Quit", "quit_minlevel") {
-			q.config.Conf.AddOption("Quit", "quit_minlevel", quit_min_auth_level)
-			log.Println("added default quit_minlevel value of \"" + quit_min_auth_level + "\" to config file")
-			// no return here either, sorry ;)
-		}
-
-		lvl_needed, err := q.config.Conf.Int("Quit", "quit_minlevel")
-		if err != nil {
-			q.ic.Reply(cmd, err.String())
-			q.config.Unlock()
-			return
-		}
-
-		if lvl_needed > lvl {
-			q.ic.Reply(cmd, "not authorized to quit this bot")
-			q.config.Unlock()
-			return
-		}
-
-		if ! q.config.Conf.HasOption("Quit", "quitmsg") {
-			log.Println("added default quitmsg value of \"" + default_quit_msg + "\" to config file")
-			q.config.Conf.AddOption("Quit", "quitmsg", default_quit_msg)
-			q.config.Unlock()
-			q.ic.Disconnect(default_quit_msg)
-		} else {
-			quitmsg, err := q.config.Conf.String("Quit", "quitmsg")
-			if err != nil {
-				q.ic.Reply(cmd, err.String())
-			}
-			q.config.Unlock()
-			q.ic.Disconnect(quitmsg)
-		}
-	*/
+	q.ic.Disconnect(q.ic.GetStringOption("Quit", "quitmsg"))
 }
 
 func (q *QuitHandler) Unregister() {
