cdb2ed343cd71f5d44b2442342ec850eb4003895
Dominik Paulus <dominik@d-paulus.de>
2012-02-14 17:49:06 +0100

    More hallowelt.go stuff

--- plugins/hallowelt.go
+++ plugins/hallowelt.go
@@ -1,10 +1,11 @@
 package plugins
 
 import (
+	"log"
 	"ircclient"
 	"http"
-	"fmt"
 	"xml"
+	"time"
 )
 
 type root struct {
@@ -36,20 +37,44 @@ type Judging struct {
 
 type HalloWeltPlugin struct {
 	ic *ircclient.IRCClient
+	done chan bool
 }
 
 func (q *HalloWeltPlugin) Register(cl *ircclient.IRCClient) {
 	q.ic = cl
 	var client http.Client
-	//response, _ := client.Get("https://bot:hallowelt@icpc.informatik.uni-erlangen.de/domjudge/plugin/event.php")
-	// Um den DOMJudge nicht uebermaessig in der Entwicklungsphase zu pollen 
-	// TODO: Konfigurierbar
-	response, _ := client.Get("http://d-paulus.de/tmp.xml")
-	fmt.Println(response.StatusCode)
-	var res root
-	xml.Unmarshal(response.Body, &res)
-	response.Body.Close()
-	fmt.Println(len(res.Events.Event))
+	go func() {
+		for {
+			// TODO: Configurable
+			t := time.After(60 * 1e9)
+			select {
+			case <- t:
+			case <- q.done:
+			q.done <- true
+			return;
+			}
+			//response, _ := client.Get("https://bot:hallowelt@icpc.informatik.uni-erlangen.de/domjudge/plugin/event.php")
+			// Um den DOMJudge nicht uebermaessig in der Entwicklungsphase zu pollen 
+			// TODO: Konfigurierbar
+			response, err := client.Get("http://d-paulus.de/tmp.xml")
+			if response.StatusCode != 200 || err != nil {
+				log.Println("ERROR: (HalloWelt): Unable to get current event list from DomJudge")
+				time.Sleep(120 * 1e9)
+				continue;
+			}
+			var res root
+			xml.Unmarshal(response.Body, &res)
+			response.Body.Close()
+			last, err := q.ic.GetIntOption("HalloWelt", "last")
+			if err != nil {
+				q.ic.SetIntOption("HalloWelt", "last", len(res.Events.Event))
+			}
+			if last == len(res.Events.Event) {
+				continue
+			}
+			// TODO: Wenn last < AnzahlEvents: Auf neue Judges pruefen und so
+		}
+	}()
 }
 
 func (q *HalloWeltPlugin) String() string {
@@ -71,4 +96,6 @@ func (q *HalloWeltPlugin) ProcessCommand(cmd *ircclient.IRCCommand) {
 }
 
 func (q *HalloWeltPlugin) Unregister() {
+	q.done <- true
+	<-q.done
 }
