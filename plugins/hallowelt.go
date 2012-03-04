4987eb045fb9f8d16af1814319023dea9a5ce250
Dominik Paulus <dominik@d-paulus.de>
2012-03-04 18:25:31 +0100

    Proof-of-concept HalloWelt parsing

--- plugins/hallowelt.go
+++ plugins/hallowelt.go
@@ -18,8 +18,8 @@ type Events struct {
 
 type Event struct {
 	Id         int `xml:"attr"`
-	Submission Submission
-	Judging    Judging
+	Submission *Submission
+	Judging    *Judging
 }
 
 type Submission struct {
@@ -44,10 +44,10 @@ func (q *HalloWeltPlugin) Register(cl *ircclient.IRCClient) {
 	q.ic = cl
 	var client http.Client
 	q.done = make(chan bool)
-	go func() {
+//	go func() {
 		for {
 			// TODO: Configurable
-			t := time.After(60 * 1e9)
+			t := time.After(1 * 1e9)
 			select {
 			case <- t:
 			case <- q.done:
@@ -61,9 +61,10 @@ func (q *HalloWeltPlugin) Register(cl *ircclient.IRCClient) {
 			if response.StatusCode != 200 || err != nil {
 				log.Println("ERROR: (HalloWelt): Unable to get current event list from DomJudge")
 				time.Sleep(120 * 1e9)
-				continue;
+				continue
 			}
 			var res root
+			// Parse XML
 			xml.Unmarshal(response.Body, &res)
 			response.Body.Close()
 			last, err := q.ic.GetIntOption("HalloWelt", "last")
@@ -73,9 +74,38 @@ func (q *HalloWeltPlugin) Register(cl *ircclient.IRCClient) {
 			if last == len(res.Events.Event) {
 				continue
 			}
-			// TODO: Wenn last < AnzahlEvents: Auf neue Judges pruefen und so
+			for i := last; i < len(res.Events.Event); i = i + 1 {
+				ev := res.Events.Event[i].Judging
+				if ev == nil {
+					continue
+				}
+				if ev.Result != "correct" {
+					continue
+				}
+				tries, team, problem := 0, "", ""
+				for i := len(res.Events.Event) - 1; i >= 0; i = i - 1 {
+					if res.Events.Event[i].Submission == nil {
+						continue
+					}
+					if res.Events.Event[i].Submission.Id == ev.Submitid {
+						tries = 1
+						team = res.Events.Event[i].Submission.Team
+						problem = res.Events.Event[i].Submission.Problem
+						continue
+					}
+					if tries != 0 && res.Events.Event[i].Submission.Problem == problem && res.Events.Event[i].Submission.Team == team {
+						tries = tries + 1
+					}
+				}
+				if tries == 0 {
+					// Ignore invalid input
+					continue
+				}
+				log.Printf("%s solved %s (after %d failed attempts)", team, problem, tries - 1)
+			}
+			log.Fatal("Bye")
 		}
-	}()
+//	}()
 }
 
 func (q *HalloWeltPlugin) String() string {
