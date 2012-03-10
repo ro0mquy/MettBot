b703a7de590c97325e29f6662c73c5f421dfe5a7
Dominik Paulus <dominik@d-paulus.de>
2012-03-10 15:09:47 +0100

    Remove client-side pingpong

--- ircclient/basicprotocol.go
+++ ircclient/basicprotocol.go
@@ -9,38 +9,10 @@ import (
 
 type basicProtocol struct {
 	ic       *IRCClient
-	timer    *time.Timer
-	lastping int64
-	done     chan bool
 }
 
 func (bp *basicProtocol) Register(cl *IRCClient) {
 	bp.ic = cl
-	bp.done = make(chan bool)
-	// Send a PING message every few minutes to detect locked-up
-	// server connection
-	go func() {
-		for {
-			sleep := time.NewTimer(30 * 60e9)
-			select {
-			case _ = <-bp.done:
-				return
-			case _ = <-sleep.C:
-			}
-			if bp.lastping != 0 {
-				continue
-			}
-			bp.lastping = time.Seconds()
-			bp.ic.conn.Output <- "PING :client"
-			bp.timer = time.NewTimer(60e9) // TODO
-			go func() {
-				select {
-				case _ = <-bp.timer.C:
-					bp.ic.Disconnect("(Client) timer expired")
-				}
-			}()
-		}
-	}()
 }
 
 func (bp *basicProtocol) String() string {
@@ -59,13 +31,9 @@ func (bp *basicProtocol) ProcessLine(msg *IRCMessage) {
 			log.Printf("WARNING: Invalid PING received")
 		}
 		bp.ic.conn.Output <- "PONG :" + msg.Args[0]
-	case "PONG":
-		bp.lastping = 0
-		bp.timer.Stop()
 	}
 }
 func (bp *basicProtocol) Unregister() {
-	bp.done <- true
 }
 
 func (bp *basicProtocol) Info() string {
@@ -73,5 +41,4 @@ func (bp *basicProtocol) Info() string {
 }
 
 func (bp *basicProtocol) ProcessCommand(cmd *IRCCommand) {
-	// TODO
 }
