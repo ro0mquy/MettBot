bcb7180ee63b9820a52800ee7b12511700e099bd
ro0mquy <git@ro0mquy.de>
2013-09-28 23:22:27 +0200

    added twitter plugin

--- main.go
+++ main.go
@@ -14,20 +14,16 @@ func main() {
 	s.RegisterPlugin(new(plugins.KexecPlugin))
 	s.RegisterPlugin(new(plugins.ListPlugins))
 	s.RegisterPlugin(plugins.NewLoggerPlugin("irclogs"))
-	//s.RegisterPlugin(new(plugins.LecturePlugin))
 	s.RegisterPlugin(new(plugins.QuitHandler))
-	s.RegisterPlugin(new(plugins.QDevoicePlugin))
 	s.RegisterPlugin(new(plugins.ChannelsPlugin))
 	s.RegisterPlugin(new(plugins.AdminPlugin))
-	//s.RegisterPlugin(new(plugins.XKCDPlugin))
-	s.RegisterPlugin(new(plugins.DecidePlugin))
-	s.RegisterPlugin(new(plugins.EvaluationPlugin))
+	s.RegisterPlugin(new(plugins.TwitterPlugin))
+
 	ok := s.Connect()
 	if ok != nil {
 		log.Fatal(ok.Error())
 	}
-	// Has to be loaded after successful connection
-	//s.RegisterPlugin(new(plugins.HalloWeltPlugin))
+
 	ok = s.InputLoop()
 	if ok != nil {
 		log.Fatal(ok.Error())
---
+++ plugins/twitter.go
@@ -0,0 +1,210 @@
+package plugins
+
+import (
+	"bytes"
+	"../ircclient"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"encoding/base64"
+	"encoding/json"
+	"html"
+	"regexp"
+	"strings"
+)
+
+const (
+	twitter_regex = `\S*twitter\.com\/\S+\/status(es)?\/(\d+)\S*`
+	twitter_api_url = `https://api.twitter.com/1.1/statuses/show.json?id=%v&trim_user=false&include_entities=true`
+)
+
+type linkurl struct {
+	Url          string
+	Expanded_url string
+}
+type entity struct {
+	Urls []linkurl
+}
+type usr struct {
+	Screen_name string
+}
+type apiError struct {
+	Message string
+	Code    int
+}
+type tweet struct {
+	Text     string
+	User     usr
+	Entities entity
+	Errors   []apiError
+}
+
+type TwitterPlugin struct {
+	ic *ircclient.IRCClient
+	regex *regexp.Regexp
+}
+
+func (q *TwitterPlugin) String() string {
+	return "twitter"
+}
+
+func (q *TwitterPlugin) Info() string {
+	return "fetches content of posted tweets"
+}
+
+func (q *TwitterPlugin) Usage(cmd string) string {
+	// just for interface saturation
+	return ""
+}
+
+func (q *TwitterPlugin) Register(cl *ircclient.IRCClient) {
+	q.ic = cl
+	q.regex = regexp.MustCompile(twitter_regex)
+}
+
+func (q *TwitterPlugin) Unregister() {
+	return
+}
+
+func (q *TwitterPlugin) ProcessLine(msg *ircclient.IRCMessage) {
+	if msg.Command != "PRIVMSG" {
+		//only process messages from chatrooms
+		return
+	}
+
+	subs := q.regex.FindStringSubmatch(msg.Args[0])
+	if subs == nil {
+		// no url to tweet in message
+		return
+	}
+
+	twt, err := q.fetchTweet(subs[2]); // second group of regex is searched tweet id
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	message := "@" + twt.User.Screen_name + ": " + twt.Text
+	var target string
+        if msg.Target != q.ic.GetStringOption("Server", "nick") {
+                target = msg.Target
+        } else {
+                target = strings.SplitN(msg.Source, "!", 2)[0]
+        }
+        q.ic.SendLine("NOTICE " + target + " :" + message)
+}
+
+func (q *TwitterPlugin) ProcessCommand(cmd *ircclient.IRCCommand) {
+	return
+}
+
+func (q *TwitterPlugin) fetchTweet(tweetId string) (twt tweet, err error) {
+	tweetUrl := fmt.Sprintf(twitter_api_url, tweetId)
+	oAuthToken := q.ic.GetStringOption("Twitter", "OAuthToken")
+	if oAuthToken == "" {
+		err = q.getOAuthToken()
+		if err != nil {
+			return
+		}
+		oAuthToken = q.ic.GetStringOption("Twitter", "OAuthToken")
+	}
+
+	request, err := http.NewRequest("GET", tweetUrl, nil)
+	request.Header.Add("Authorization", "Bearer " + oAuthToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &twt)
+	if err != nil {
+		return
+	}
+
+	for _, e := range twt.Errors {
+		if e.Code == 89 {
+			err = fmt.Errorf("Twitter API returned: %s", e.Message)
+			q.ic.RemoveOption("Twitter", "OAuthToken")
+			return
+		}
+	}
+
+	for _, u := range twt.Entities.Urls {
+		twt.Text = strings.Replace(twt.Text, u.Url, u.Expanded_url, -1)
+	}
+	twt.Text = html.UnescapeString(twt.Text)
+
+	twt.Text = strings.Map(func(r rune) rune {
+		if r < ' ' {
+			return ' '
+		}
+		return r
+	}, twt.Text)
+
+	if twt.User.Screen_name == "" || twt.Text == "" {
+		err = fmt.Errorf("Error in tweet:\n%v\nUser: %v\nTweet text: %v", string(body), twt.User.Screen_name, twt.Text)
+		return
+	}
+
+	return twt, nil
+}
+
+func (q *TwitterPlugin) getOAuthToken() (err error) {
+	if q.ic.GetStringOption("Twitter", "OAuthToken") != "" {
+		return nil
+	}
+
+	key := q.ic.GetStringOption("Twitter", "key")
+	secret := q.ic.GetStringOption("Twitter", "secret")
+	if key == "" || secret == "" {
+		err = fmt.Errorf("To fetch tweets, you have to specify the key and a secret of your application")
+		return
+	}
+
+	toBase64 := []byte(key + ":" + secret)
+	encodedKey := base64.StdEncoding.EncodeToString(toBase64)
+	bufferHTTPBody := bytes.NewBufferString("grant_type=client_credentials")
+
+	requestOAuth, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", bufferHTTPBody)
+	requestOAuth.Header.Add("Authorization", "Basic "+encodedKey)
+	requestOAuth.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+
+	client := &http.Client{}
+	responseOAuth, err := client.Do(requestOAuth)
+	if err != nil {
+		return
+	}
+
+	defer responseOAuth.Body.Close()
+	bodyOAuth, err := ioutil.ReadAll(responseOAuth.Body)
+	if err != nil {
+		return
+	}
+
+	type oauthAnswer struct {
+		Token_type   string
+		Access_token string
+	}
+
+	var answer oauthAnswer
+	err = json.Unmarshal(bodyOAuth, &answer)
+	if err != nil {
+		return
+	}
+	if answer.Token_type != "bearer" {
+		err = fmt.Errorf("False token type from Twitter OAuth API: %v", answer.Token_type)
+		return
+	}
+
+	q.ic.SetStringOption("Twitter", "OAuthToken", answer.Access_token)
+	return nil
+}
