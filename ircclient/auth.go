9a41f3ae621d788ebcd7e71ac4e090fbb58457d0
ro0mquy <git@ro0mquy.de>
2013-10-01 00:37:29 +0200

    added delacces command and fixed variuos bugs in auth plugin

--- ircclient/auth.go
+++ ircclient/auth.go
@@ -24,7 +24,8 @@ func (a *authPlugin) Register(cl *IRCClient) {
 	}
 	a.ic.RegisterCommandHandler("mya", 0, 0, a)
 	a.ic.RegisterCommandHandler("myaccess", 0, 0, a)
-	a.ic.RegisterCommandHandler("addaccess", 0, 400, a)
+	a.ic.RegisterCommandHandler("addaccess", 2, 400, a)
+	a.ic.RegisterCommandHandler("delaccess", 1, 400, a)
 }
 func (a *authPlugin) String() string {
 	return "auth"
@@ -38,6 +39,8 @@ func (a *authPlugin) Usage(cmd string) string {
 		return cmd + ": tells you what access-level (i.e. permissions) you have"
 	case "addaccess":
 		return "addaccess <hostmask> <level>: adds access-level <level> for hostmask <hostmask>"
+	case "delaccess":
+		return "delaccess <hostmask>: removes access-level for hostmask <hostmask>"
 	}
 	// shouldn't be a problem, this usage isn't called unless we're registered for it
 	return ""
@@ -64,41 +67,41 @@ func (a *authPlugin) ProcessCommand(cmd *IRCCommand) {
 		} else {
 			a.ic.Reply(cmd, "Your access level is: "+slevel)
 		}
+
 	case "addaccess":
-		level := a.GetAccessLevel(cmd.Source)
-		newlevel, err := strconv.Atoi(cmd.Args[1])
-		if err != nil {
-			a.ic.Reply(cmd, "Error: "+err.Error())
-		}
-		if level < newlevel || level < 400 {
-			a.ic.Reply(cmd, "You are not authorized to do this")
-			return
-		}
 		if _, err := regexp.Compile(cmd.Args[0]); err != nil {
 			a.ic.Reply(cmd, "Error: Unable to compile regexp: "+err.Error())
 			return
 		}
-		//a.ic.SetIntOption("Auth", cmd.Args[0], newlevel)
-		a.SetAccessLevel(cmd.Args[0], newlevel)
-		a.ic.Reply(cmd, "Permissions granted")
-	case "delaccess":
-		if len(cmd.Args) != 1 {
-			a.ic.Reply(cmd, "delaccess takes mask to delete as an argument")
+
+		userLevel := a.GetAccessLevel(cmd.Source)
+		targetLevel, _ := a.ic.GetIntOption("Auth", cmd.Args[0])
+		newLevel, err := strconv.Atoi(cmd.Args[1])
+		if err != nil {
+			a.ic.Reply(cmd, "Error: "+err.Error())
+		}
+
+		if userLevel < newLevel || userLevel <= targetLevel {
+			a.ic.Reply(cmd, "You are not authorized to do this")
 			return
 		}
+		a.SetAccessLevel(cmd.Args[0], newLevel)
+		a.ic.Reply(cmd, "Permissions granted")
+
+	case "delaccess":
 		level := a.GetAccessLevel(cmd.Source)
-		dlevel, ok := a.ic.GetIntOption("Auth", cmd.Args[0])
-		if ok != nil {
-			if dlevel >= level || level != 500 {
-				a.ic.Reply(cmd, "Can't remove mask: Has higher privileges than you")
-				return
-			}
-			//a.ic.RemoveOption("Auth", cmd.Args[0])
-			a.DelAccessLevel(cmd.Args[0])
-			a.ic.Reply(cmd, "Successfully removed mask")
-		} else {
+		dlevel, err := a.ic.GetIntOption("Auth", cmd.Args[0])
+		if err != nil {
 			a.ic.Reply(cmd, "Mask not found")
+			return
 		}
+
+		if dlevel >= level {
+			a.ic.Reply(cmd, "Can't remove mask: Has higher privileges than you")
+			return
+		}
+		a.DelAccessLevel(cmd.Args[0])
+		a.ic.Reply(cmd, "Successfully removed mask")
 	}
 }
 
